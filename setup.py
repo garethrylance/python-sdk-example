3f21c3a6c28f46050688ce1be66e33433a801e7c
Gareth Rylance <gareth.rylance@auroraer.com>
2020-06-15 09:39:52 +0100

    rename in setup.py

--- setup.py
+++ setup.py
@@ -7,20 +7,11 @@ setup(
     url="https://github.com/garethrylance/python-sdk-example",
     author="Gareth Rylance",
     author_email="gareth@rylance.me.uk",
-    packages=["example-sdk"],
-    install_requires=[
-        "pandas",
-    ],
+    packages=["example_sdk"],
+    install_requires=["pandas"],
     zip_safe=False,
     entry_points={"console_scripts": [""]},
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
-    extras_require={
-        "development": [
-            "flake8",
-            "black",
-            "pytest",
-            "snapshottest",
-        ]
-    },
+    extras_require={"development": ["flake8", "black", "pytest", "snapshottest"]},
 )
